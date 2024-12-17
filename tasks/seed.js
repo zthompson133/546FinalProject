002cc47b77773c86eaa6c675dead3834f9ccef12
Mehtaab Singh <mehtaabsingh01@gmail.com>
2024-12-16 22:42:27 -0500

    Create seed to create one verified and two unverified users as well as two events

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,27 +1,48 @@
 import {dbConnection, closeConnection} from '../config/mongoConnection.js';
-import events from '../data/events.js';
-import users from '../data/users.js';
+import * as event from '../data/events.js';
+import * as user from '../data/users.js';
+import {users} from '../config/mongoCollections.js';
+import {ObjectId} from "mongodb";
 
 const db = await dbConnection();
 await db.dropDatabase();
 
-const user1 = await users.addUser('Mark', 'Johnson', 'mjohnson@stevens.edu', 'Undergraduate', 'helloWorld/1', 'helloWorld/1');
-const mid = user1._id.toString();
+const userCollection = await users();
 
-const user2 = await users.addUser('Sara', 'Miller', 'smiller@stevens.edu', 'Undergraduate', 'cs4All!', 'cs4All!');
-const sid = await user2._id.toString();
+const user1 = await user.addUser('Mark', 'Johnson', 'mjohnson@stevens.edu', 'Undergraduate', 'helloWorld/1', 'helloWorld/1');
 
-const vUser = await users.addUser('Joseph', 'Walker', 'jwalker@stevens.edu', 'Undergraduate', 'eventOrg#2', 'eventOrg#2');
-const vid = await vUser._id.toString();
-
-const firstEvent = await events.addEvent(
-  'Arts and Crafts', 'Join us for crafting and free food', '12/22/24', '6:00 pm', '8:00 pm', 'Babbio 104', 'Undergraduates',
-  mid, sid, vid
+const userUpdate1 = await userCollection.updateOne(
+    { _id: new ObjectId(user1._id.toString()) },
+    { $set: { verified: true, emailVerified: true } }
 );
 
-const secondEvent = await events.addEvent(
-  'Movie Night', 'Join us for a movie and free food', '12/20/24', '7:00 pm', '9:00 pm', 'McLean 104', 'Undergraduates',
-  mid, sid, vid
+const mid = await user.getUserById(user1._id.toString());
+const midEmail = mid.email
+
+const user2 = await user.addUser('Sara', 'Miller', 'smiller@stevens.edu', 'Undergraduate', 'cs4All!!', 'cs4All!!');
+
+const userUpdate2 = await userCollection.updateOne(
+  { _id: new ObjectId(user2._id.toString()) },
+  { $set: { emailVerified: true } }
+);
+
+const sid = await user.getUserById(user2._id.toString());
+
+const user3 = await user.addUser('Joseph', 'Walker', 'jwalker@stevens.edu', 'Undergraduate', 'eventOrg#2', 'eventOrg#2');
+
+const userUpdate3 = await userCollection.updateOne(
+  { _id: new ObjectId(user3._id.toString()) },
+  { $set: { emailVerified: true } }
+);
+
+const vid = user.getUserById(user3._id.toString());
+
+const firstEvent = await event.addEvent(
+  'Arts and Crafts', 'Join us for crafting and free food', '2024-22-12', '6:00', '8:00', 'Babbio 104', midEmail, 'undergraduate'
+);
+
+const secondEvent = await event.addEvent(
+  'Movie Night', 'Join us for a movie and free food', '2024-20-12', '17:00', '19:00', 'McLean 104', midEmail, 'graduate',
 );
 
 console.log('Done seeding database');
